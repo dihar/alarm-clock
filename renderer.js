d10a944f98ba2bda038e67fa8160b7f2f32034d8
dihar <dihar@gmail.com>
2016-07-31 16:06:39 +0300

    first commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+document.querySelector('alarm-app').addEventListener('resolve', onResolve);
+function onResolve(){
+	document.querySelector('.md-preloader').remove();
+};
