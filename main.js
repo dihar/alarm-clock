e1ecaf102e524db05867a2b4f6f3d7f8c8ec0266
dihar <dihar@gmail.com>
2016-09-06 08:39:59 +0300

    beta version

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1100, height: 700})
+  mainWindow = new BrowserWindow({width: 700, height: 700})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
